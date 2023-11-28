4a42ed0fc1bd41d0ff10c7ca622fad845e2ba436
eduran8291 <eduran8291@ceuandalucia.es>
2023-11-28 19:27:58 +0100

    Cambio en la Clase3_3

--- Proyecto3_3/src/PracticaGit/Clase3_3.java
+++ Proyecto3_3/src/PracticaGit/Clase3_3.java
@@ -3,7 +3,7 @@ package PracticaGit;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("Practica3_3" + " " + "Enrique Durán Malaver");
+		System.out.println("CanelaEnRama");
 
 	}
 
